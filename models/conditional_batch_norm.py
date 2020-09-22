683b30fef8df5b3a40b2a0b2ff71574903b21a13
lvassertail <58955355+lvassertail@users.noreply.github.com>
2020-09-22 21:00:54 +0300

    use pytorch ConditionalBatchNorm

--- models/conditional_batch_norm.py
+++ models/conditional_batch_norm.py
@@ -1,77 +1,23 @@
+from torch.nn import BatchNorm2d
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.utils.data
 
 
-class BatchNorm2d(nn.BatchNorm2d):
-    def reset_parameters(self):
-        self.reset_running_stats()
-        if self.affine:
-            self.weight.data.fill_(1.0)
-            self.bias.data.zero_()
+class ConditionalBatchNorm(torch.nn.Module):
 
-
-class CategoricalConditionalBatchNorm(nn.Module):
-    # as in the chainer SN-GAN implementation, we keep per-cat weight and bias
-    def __init__(self, num_features, num_cats, eps=2e-5, momentum=0.1, affine=True,
+    def __init__(self, num_features, num_categories, eps=1e-5, momentum=0.1, affine=False,
                  track_running_stats=True):
         super().__init__()
-        self.num_features = num_features
-        self.num_cats = num_cats
-        self.eps = eps
-        self.momentum = momentum
-        self.affine = affine
-        self.track_running_stats = track_running_stats
-        if self.affine:
-            self.weight = nn.Parameter(torch.Tensor(num_cats, num_features))
-            self.bias = nn.Parameter(torch.Tensor(num_cats, num_features))
-        else:
-            self.register_parameter('weight', None)
-            self.register_parameter('bias', None)
-        if self.track_running_stats:
-            self.register_buffer('running_mean', torch.zeros(num_features))
-            self.register_buffer('running_var', torch.ones(num_features))
-            self.register_buffer('num_batches_tracked', torch.tensor(0, dtype=torch.long))
-        else:
-            self.register_parameter('running_mean', None)
-            self.register_parameter('running_var', None)
-            self.register_parameter('num_batches_tracked', None)
-        self.reset_parameters()
+        self.batch_norm = BatchNorm2d(num_features, eps, momentum, affine, track_running_stats)
+        self.gamma_c = torch.nn.Embedding(num_categories, num_features)
+        self.beta_c = torch.nn.Embedding(num_categories, num_features)
+        torch.nn.init.constant_(self.batch_norm.running_var.data, 0)
+        torch.nn.init.constant_(self.gamma_c.weight.data, 1)
+        torch.nn.init.constant_(self.beta_c.weight.data, 0)
 
-    def reset_running_stats(self):
-        if self.track_running_stats:
-            self.running_mean.zero_()
-            self.running_var.fill_(1)
-            self.num_batches_tracked.zero_()
-
-    def reset_parameters(self):
-        self.reset_running_stats()
-        if self.affine:
-            self.weight.data.fill_(1.0)
-            self.bias.data.zero_()
-
-    def forward(self, input, cats):
-        exponential_average_factor = 0.0
-
-        if self.training and self.track_running_stats:
-            self.num_batches_tracked += 1
-            if self.momentum is None:  # use cumulative moving average
-                exponential_average_factor = 1.0 / self.num_batches_tracked.item()
-            else:  # use exponential moving average
-                exponential_average_factor = self.momentum
-
-        out = F.batch_norm(
-            input, self.running_mean, self.running_var, None, None,
-            self.training or not self.track_running_stats,
-            exponential_average_factor, self.eps)
-        if self.affine:
-            shape = [input.size(0), self.num_features] + (input.dim() - 2) * [1]
-            weight = self.weight.index_select(0, cats).view(shape)
-            bias = self.bias.index_select(0, cats).view(shape)
-            out = out * weight + bias
-        return out
-
-    def extra_repr(self):
-        return '{num_features}, num_cats={num_cats}, eps={eps}, momentum={momentum}, affine={affine}, ' \
-               'track_running_stats={track_running_stats}'.format(**self.__dict__)
+    def forward(self, input, y):
+        ret = self.batch_norm(input)
+        gamma = self.gamma_c(y)
+        beta = self.beta_c(y)
+        gamma_b = gamma.unsqueeze(2).unsqueeze(3).expand_as(ret)
+        beta_b = beta.unsqueeze(2).unsqueeze(3).expand_as(ret)
+        return gamma_b*ret + beta_b
